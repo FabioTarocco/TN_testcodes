a5d0532174e58f0644a434a4990b25db294e936e
FabioTarocco <fabio.tarocco.vr@gmail.com>
2023-09-25 17:08:26 +0200

    Dissociaton of H2

--- codes/dmrg_qqc.jl
+++ codes/dmrg_qqc.jl
@@ -65,6 +65,7 @@ println("Number of qubit operators = ", length(qubit_hamiltonian));
 println("|HF⟩ = |", prod(string.(qubit_state)), "⟩");
 
 #=
+----------------- TEST DOUBLE TYPE---------------------
 Electron Hamiltonian {vac, up, dn, updn} for each sites
 =#
 
@@ -76,10 +77,13 @@ hf = molecular_orbital_hamiltonian(molecule; basis)
 hamiltonian = hf.hamiltonian
 hartree_fock_state = hf.hartree_fock_state
 hartree_fock_energy = hf.hartree_fock_energy
+nterms = length(hamiltonian)
+println("Number of terms in the OpSum: $nterms")
 
 s = siteinds("Electron", length(hartree_fock_state); conserve_qns=true)
 println("\n$s",s)
 H = MPO(hamiltonian, s)
+@show maxlinkdim(H)
 ψhf = MPS(s, hartree_fock_state)
 
 inner(ψhf', H, ψhf) ≈ hartree_fock_energy
@@ -88,7 +92,7 @@ sweeps = Sweeps(10)
 setmaxdim!(sweeps, 100, 200)
 setcutoff!(sweeps, 1e-6)
 setnoise!(sweeps, 1e-6, 1e-7, 1e-8, 0.0)
-energy_dmrg_electronic, psi_dmrg_electronic= @time dmrg(H, ψhf, sweeps; outputlevel=0)
+energy_dmrg_electronic, psi_dmrg_electronic= dmrg(H, ψhf, sweeps; outputlevel=0)
 @show energy_dmrg_electronic < hartree_fock_energy
 
 #=
@@ -100,6 +104,7 @@ molecule = Molecule("H₂")
 basis = "sto-3g"
 hf = molecular_orbital_hamiltonian(molecule; basis)
 hamiltonian = hf.hamiltonian
+nterms_e = length(hamiltonian)
 hartree_fock_state = hf.hartree_fock_state
 hartree_fock_energy = hf.hartree_fock_energy
 
@@ -108,16 +113,19 @@ He = MPO(hamiltonian, se)
 ψe = MPS(se, hartree_fock_state)
 
 inner(ψe', He, ψe) ≈ hartree_fock_energy
+@show inner(ψe', He, ψe) - hartree_fock_energy
 
 
 #HERE I select the sitetype of the system
 hf = molecular_orbital_hamiltonian(molecule; basis, sitetype="Fermion")
 hamiltonian = hf.hamiltonian
+nterms_f = length(hamiltonian)
 hartree_fock_state = hf.hartree_fock_state
 hartree_fock_energy = hf.hartree_fock_energy
 
-sf = siteinds("Fermion", length(hartree_fock_state); conserve_qns=false)
+sf = siteinds("Fermion", length(hartree_fock_state); conserve_qns=true)
 Hf = MPO(hamiltonian, sf)
+@show maxlinkdim(Hf)
 ψf = MPS(sf, hartree_fock_state)
 
 inner(ψf', Hf, ψf) ≈ hartree_fock_energy
@@ -134,4 +142,60 @@ setmaxdim!(sweeps, 100, 200)
 setcutoff!(sweeps, 1e-10)
 setnoise!(sweeps, 1e-6, 1e-7, 1e-8, 0.0)
 Ef, ψf = dmrg(Hf, ψf, sweeps; outputlevel=0)
-@test Ee ≈ Ef
+Ee ≈ Ef
+
+
+#=
+------------CHECK MAPPINGS----------
+=#
+molecule = Molecule("H₂O")
+basis = "sto-3g"
+hf = molecular_orbital_hamiltonian(molecule; basis)
+hamiltonian = hf.hamiltonian
+@show length(hamiltonian)
+hartree_fock_state = hf.hartree_fock_state
+hartree_fock_energy = hf.hartree_fock_energy
+
+electron_hilbert = siteinds("Electron", length(hartree_fock_state); conserve_qns=true)
+He = MPO(hamiltonian, electron_hilbert)
+ψ₀e = MPS(electron_hilbert, hartree_fock_state)
+inner(ψ₀e', He, ψ₀e) ≈ hartree_fock_energy
+electronE = copy(hartree_fock_energy)
+
+
+hf = molecular_orbital_hamiltonian(molecule; basis, sitetype="Fermion")
+hamiltonian = hf.hamiltonian
+@show length(hamiltonian)
+hartree_fock_state = hf.hartree_fock_state
+hartree_fock_energy = hf.hartree_fock_energy
+
+fermion_hilbert = siteinds("Fermion", length(hartree_fock_state); conserve_qns=true)
+Hf = MPO(hamiltonian, fermion_hilbert)
+ψ₀f = MPS(fermion_hilbert, hartree_fock_state)
+inner(ψ₀f', Hf, ψ₀f) ≈ electronE
+
+
+
+hf = molecular_orbital_hamiltonian(molecule; basis, sitetype="Qubit")
+hamiltonian = hf.hamiltonian
+@show length(hamiltonian)
+hartree_fock_state = hf.hartree_fock_state
+hartree_fock_energy = hf.hartree_fock_energy
+
+qubit_hilbert = siteinds("Qubit", length(hartree_fock_state); conserve_qns=true)
+Hq = MPO(hamiltonian, qubit_hilbert)
+ψ₀q = MPS(qubit_hilbert, hartree_fock_state)
+inner(ψ₀q', Hq, ψ₀q) ≈ electronE
+
+sweeps = Sweeps(10)
+setmaxdim!(sweeps, 100, 200)
+setcutoff!(sweeps, 1e-10)
+setnoise!(sweeps, 1e-6, 1e-7, 1e-8, 0.0)
+
+Ee, _ = dmrg(He, ψ₀e, sweeps; outputlevel=0)
+Ef, _ = dmrg(Hf, ψ₀f, sweeps; outputlevel=0)
+Eq, _ = dmrg(Hq, ψ₀q, sweeps; outputlevel=0)
+
+
+Ee ≈ Ef
+Ee ≈ Eq
